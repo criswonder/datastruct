e10fc0bcab7a17401e79493c6c0ea06301a23d81
hongyun <hongyun.mhy@alibaba-inc.com>
2020-07-03 15:59:39 +0800

    test queue

--- src/test/java/com/liujun/datastruct/base/datastruct/queue/leetcode/code703/TestKthLargest.java
+++ src/test/java/com/liujun/datastruct/base/datastruct/queue/leetcode/code703/TestKthLargest.java
@@ -17,13 +17,13 @@ public class TestKthLargest {
     int[] arrs = new int[] {1, 2, 4, 2, 5, 7, 2, 4, 10, 289};
 
     KthLargest large = new KthLargest(6, arrs);
-
-    for (int i = 0; i < 200; i++) {
-      System.out.print(large.add(RandomUtils.getNextInt(999999)) + "\t");
-      //  System.out.print(large.add(i) + "\t");
-      if (i % 10 == 0) {
-        System.out.println();
-      }
-    }
+    System.out.println(large.add(1));
+//    for (int i = 0; i < 200; i++) {
+//      System.out.print(large.add(RandomUtils.getNextInt(999999)) + "\t");
+//      //  System.out.print(large.add(i) + "\t");
+//      if (i % 10 == 0) {
+//        System.out.println();
+//      }
+//    }
   }
 }
